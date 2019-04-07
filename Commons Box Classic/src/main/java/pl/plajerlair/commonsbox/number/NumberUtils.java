73645c45921e9cf998f8125c363781cc64655c80
Plajer <plajer.xyz@gmail.com>
2019-04-07 14:46:15 +0200

    Fixed exp saving and the rest

--- Commons Box Classic/src/main/java/pl/plajerlair/commonsbox/number/NumberUtils.java
+++ Commons Box Classic/src/main/java/pl/plajerlair/commonsbox/number/NumberUtils.java
@@ -1,5 +1,8 @@
 package pl.plajerlair.commonsbox.number;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * @author Plajer
  * <p>
@@ -23,6 +26,24 @@ public class NumberUtils {
     } catch (NumberFormatException ex) {
       return false;
     }
+  }
+
+  /**
+   * Rounds value to x places
+   *
+   * @param value  value to round
+   * @param places places
+   * @return rounded value
+   * @throws IllegalArgumentException when places to round is lower than 0
+   */
+  public static double round(double value, int places) {
+    if (places < 0) {
+      throw new IllegalArgumentException();
+    }
+
+    BigDecimal bd = new BigDecimal(value);
+    bd = bd.setScale(places, RoundingMode.HALF_UP);
+    return bd.doubleValue();
   }
 
 }
--- Commons Box Minecraft/src/main/java/pl/plajerlair/commonsbox/minecraft/serialization/InventorySerializer.java
+++ Commons Box Minecraft/src/main/java/pl/plajerlair/commonsbox/minecraft/serialization/InventorySerializer.java
@@ -1,5 +1,9 @@
 package pl.plajerlair.commonsbox.minecraft.serialization;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
@@ -15,10 +19,6 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Plajer
@@ -40,60 +40,60 @@ public class InventorySerializer {
   public static boolean saveInventoryToFile(JavaPlugin plugin, Player player) {
     String UUID = player.getUniqueId().toString();
     PlayerInventory inventory = player.getInventory();
-    File path = new File(plugin.getDataFolder() + File.separator + "inventorys");
-    if(inventory == null) {
+    File path = new File(plugin.getDataFolder() + File.separator + "inventories");
+    if (inventory == null) {
       return false;
     }
     try {
-      File invFile = new File(plugin.getDataFolder() + File.separator + "inventorys" + File.separator, UUID + ".invsave");
-      if(!path.exists()) {
+      File invFile = new File(plugin.getDataFolder() + File.separator + "inventories" + File.separator, UUID + ".invsave");
+      if (!path.exists()) {
         path.mkdir();
       }
-      if(invFile.exists()) {
+      if (invFile.exists()) {
         invFile.delete();
       }
       FileConfiguration invConfig = YamlConfiguration.loadConfiguration(invFile);
 
-      invConfig.set("Exp", player.getExp());
-      invConfig.set("ExpLevel", player.getLevel());
+      invConfig.set("ExperienceProgress", player.getExp());
+      invConfig.set("ExperienceLevel", player.getLevel());
       invConfig.set("Current health", player.getHealth());
       invConfig.set("Max health", player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue());
       invConfig.set("Food", player.getFoodLevel());
       invConfig.set("Saturation", player.getSaturation());
       invConfig.set("Fire ticks", player.getFireTicks());
-      invConfig.set("GameMode", player.getGameMode().toString());
+      invConfig.set("GameMode", player.getGameMode().name());
       invConfig.set("Allow flight", player.getAllowFlight());
 
       invConfig.set("Size", inventory.getSize());
       invConfig.set("Max stack size", inventory.getMaxStackSize());
       List<String> activePotions = new ArrayList<>();
-      for(PotionEffect potion : player.getActivePotionEffects()) {
+      for (PotionEffect potion : player.getActivePotionEffects()) {
         activePotions.add(potion.getType().getName() + "#" + potion.getDuration() + "#" + potion.getAmplifier());
       }
       invConfig.set("Active potion effects", activePotions);
-      if(inventory.getHolder() instanceof Player) {
+      if (inventory.getHolder() instanceof Player) {
         invConfig.set("Holder", (inventory.getHolder()).getName());
       }
 
       ItemStack[] invContents = inventory.getContents();
-      for(int i = 0; i < invContents.length; i++) {
+      for (int i = 0; i < invContents.length; i++) {
         ItemStack itemInInv = invContents[i];
-        if(itemInInv != null && itemInInv.getType() != Material.AIR) {
+        if (itemInInv != null && itemInInv.getType() != Material.AIR) {
           invConfig.set("Slot " + i, itemInInv);
         }
       }
 
       ItemStack[] armorContents = inventory.getArmorContents();
-      for(int b = 0; b < armorContents.length; b++) {
+      for (int b = 0; b < armorContents.length; b++) {
         ItemStack itemStack = armorContents[b];
-        if(itemStack != null && itemStack.getType() != Material.AIR) {
+        if (itemStack != null && itemStack.getType() != Material.AIR) {
           invConfig.set("Armor " + b, itemStack);
         }
       }
 
       invConfig.save(invFile);
       return true;
-    } catch(Exception ex) {
+    } catch (Exception ex) {
       ex.printStackTrace();
       Bukkit.getConsoleSender().sendMessage("Cannot save inventory of player!");
       Bukkit.getConsoleSender().sendMessage("Disable inventory saving option in config.yml or restart the server!");
@@ -102,26 +102,25 @@ public class InventorySerializer {
   }
 
   private static Inventory getInventoryFromFile(JavaPlugin plugin, String UUID) {
-    File file = new File(plugin.getDataFolder() + File.separator + "inventorys" + File.separator + UUID + ".invsave");
-    if(!file.exists() || file.isDirectory() || !file.getAbsolutePath().endsWith(".invsave")) {
+    File file = new File(plugin.getDataFolder() + File.separator + "inventories" + File.separator + UUID + ".invsave");
+    if (!file.exists() || file.isDirectory() || !file.getAbsolutePath().endsWith(".invsave")) {
       return Bukkit.createInventory(null, 9);
     }
     try {
       FileConfiguration invConfig = YamlConfiguration.loadConfiguration(file);
       Inventory inventory;
-      int invTitle = invConfig.getInt("Exp");
       int invSize = invConfig.getInt("Size", 36);
       int invMaxStackSize = invConfig.getInt("Max stack size", 64);
       InventoryHolder invHolder = null;
-      if(invConfig.contains("Holder")) {
+      if (invConfig.contains("Holder")) {
         invHolder = Bukkit.getPlayer(invConfig.getString("Holder"));
       }
-      inventory = Bukkit.getServer().createInventory(invHolder, InventoryType.PLAYER, Integer.toString(invTitle));
+      inventory = Bukkit.getServer().createInventory(invHolder, InventoryType.PLAYER);
       inventory.setMaxStackSize(invMaxStackSize);
       try {
         ItemStack[] invContents = new ItemStack[invSize];
-        for(int i = 0; i < invSize; i++) {
-          if(invConfig.contains("Slot " + i)) {
+        for (int i = 0; i < invSize; i++) {
+          if (invConfig.contains("Slot " + i)) {
             invContents[i] = invConfig.getItemStack("Slot " + i);
           } else {
             invContents[i] = new ItemStack(Material.AIR);
@@ -129,14 +128,14 @@ public class InventorySerializer {
         }
         inventory.setContents(invContents);
 
-      } catch(Exception ex) {
+      } catch (Exception ex) {
         ex.printStackTrace();
         Bukkit.getConsoleSender().sendMessage("Cannot save inventory of player! Could not get armor!");
         Bukkit.getConsoleSender().sendMessage("Disable inventory saving option in config.yml or restart the server!");
       }
       file.delete();
       return inventory;
-    } catch(Exception ex) {
+    } catch (Exception ex) {
       ex.printStackTrace();
       Bukkit.getConsoleSender().sendMessage("Cannot save inventory of player!");
       Bukkit.getConsoleSender().sendMessage("Disable inventory saving option in config.yml or restart the server!");
@@ -151,16 +150,16 @@ public class InventorySerializer {
    * @param player load inventory of this player
    */
   public static void loadInventory(JavaPlugin plugin, Player player) {
-    File file = new File(plugin.getDataFolder() + File.separator + "inventorys" + File.separator + player.getUniqueId().toString() + ".invsave");
-    if(!file.exists() || file.isDirectory() || !file.getAbsolutePath().endsWith(".invsave")) {
+    File file = new File(plugin.getDataFolder() + File.separator + "inventories" + File.separator + player.getUniqueId().toString() + ".invsave");
+    if (!file.exists() || file.isDirectory() || !file.getAbsolutePath().endsWith(".invsave")) {
       return;
     }
     try {
       FileConfiguration invConfig = YamlConfiguration.loadConfiguration(file);
       try {
         ItemStack[] armor = new ItemStack[player.getInventory().getArmorContents().length];
-        for(int i = 0; i < player.getInventory().getArmorContents().length; i++) {
-          if(invConfig.contains("Armor " + i)) {
+        for (int i = 0; i < player.getInventory().getArmorContents().length; i++) {
+          if (invConfig.contains("Armor " + i)) {
             armor[i] = invConfig.getItemStack("Armor " + i);
           } else {
             armor[i] = new ItemStack(Material.AIR);
@@ -168,34 +167,33 @@ public class InventorySerializer {
         }
         player.getInventory().setArmorContents(armor);
         player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(invConfig.getDouble("Max health"));
+        player.setExp(0);
+        player.setLevel(0);
+        player.setLevel(invConfig.getInt("ExperienceLevel"));
+        player.setExp(Float.parseFloat(invConfig.getString("ExperienceProgress")));
         player.setHealth(invConfig.getDouble("Current health"));
         player.setFoodLevel(invConfig.getInt("Food"));
-        player.setSaturation((float) invConfig.get("Saturation"));
+        player.setSaturation(Float.parseFloat(invConfig.getString("Saturation")));
         player.setFireTicks(invConfig.getInt("Fire ticks"));
         player.setGameMode(GameMode.valueOf(invConfig.getString("GameMode")));
         player.setAllowFlight(invConfig.getBoolean("Allow flight"));
         List<String> activePotions = invConfig.getStringList("Active potion effects");
-        for(String potion : activePotions) {
+        for (String potion : activePotions) {
           String[] splited = potion.split("#");
           player.addPotionEffect(new PotionEffect(PotionEffectType.getByName(splited[0]), Integer.valueOf(splited[1]), Integer.valueOf(splited[2])));
         }
-      } catch(Exception ignored) {
+      } catch (Exception ignored) {
       }
       Inventory inventory = getInventoryFromFile(plugin, player.getUniqueId().toString());
 
-      for(int i = 0; i < inventory.getContents().length; i++) {
-        if(inventory.getItem(i) != null) {
+      for (int i = 0; i < inventory.getContents().length; i++) {
+        if (inventory.getItem(i) != null) {
           player.getInventory().setItem(i, inventory.getItem(i));
         }
       }
 
       player.updateInventory();
-
-      try {
-        player.setLevel(invConfig.getInt("ExpLevel"));
-        player.setExp((float) invConfig.getDouble("Exp"));
-      } catch(Exception ignored) {}
-    } catch(Exception ignored) {
+    } catch (Exception ignored) {
     }
   }
 
